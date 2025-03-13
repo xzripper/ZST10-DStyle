1c9fb7b6d03c17f3c264d2fc4c40c255d4f2dccb
Évan 🌿 <94743980+xzripper@users.noreply.github.com>
2025-03-13 01:21:17 +0100

    Create test.d

---
+++ test.d
@@ -0,0 +1,59 @@
+import std.stdio;
+
+// import MyModule.SomethingImportant;
+
+// Import Order: STD -> Third-Party -> Project
+// Visibility: Private (default)
+// Identantion:   spaces
+// Line length: NO MAX
+// Braces: K&R (bigger projects) or 1TBS/OTBS
+// Blank lines: Split it between blocks in code (E.G SOMETHING_MASSIVE [2 BLANK LINES] ETC) or not used at all.
+// DDOC: ///
+// Inline comments: //
+// Error handling: Exceptions/Exceptions based on checks
+
+void PrintMe()
+{
+    writeln("PrintMe");
+}
+
+struct MyStruct
+{
+    string m_StructMember;
+
+    string GetStructMember() {
+        return m_StructMember;
+    }
+}
+
+class MyClass
+{
+    string m_ClassMember;
+
+    string GetClassMember() {
+        return m_ClassMember;
+    }
+}
+
+interface MyInterface
+{
+    string Foo();
+}
+
+enum MyEnum
+{
+    WhateverOne,
+    WhateverTwo,
+    WhateverThree
+}
+
+void main()
+{
+    string myVariable;
+    string g_MyVariable; // Global variables for future usage.
+    const string MY_CONST;
+    immutable string IM_MY_CONST;
+    alias al_Integer = int;
+
+    PrintMe();
+}
